46528bb253a1cbf2e5bf9b693dd03003edb97870
Zeeshanahmad4 <ahmadzee26@gmail.com>
2018-10-23 18:48:46 +0500

    Add files via upload

---
+++ Django.py
@@ -0,0 +1,30 @@
+# making a virtual env for dejango
+
+
+#comands for windows
+
+ pip install virtualenv                     #install virtual env
+
+
+ mkdir dir_name
+ cd dir_name
+
+
+virtualenv -p python3                       #make dir virtualenv
+
+
+
+.\Scripts\activate                           #activating env
+
+
+
+
+pip install django==1.11.5                  #installing django
+
+
+
+
+django-admin startproject project_name #starting your first project
+
+
+python3 manage.py runserver #run this to see your project on server make sure you are in right directory
