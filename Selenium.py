80ee44d9c4a6a3d98be07560907e4bb0bd3d74cf
Zeeshanahmad4 <ahmadzee26@gmail.com>
2019-02-19 00:01:03 -0500

    Update Selenium.py

--- Selenium.py
+++ Selenium.py
@@ -142,3 +142,14 @@ for i in post_elems:
     
 #getting url of current tab 
 print browser.current_url
+
+
+
+#hidden elements that gives null values
+    html = browser.page_source
+    soup = BeautifulSoup(html)
+    divTag = soup.find_all("yt-formatted-string", {"class": "style-scope ytd-toggle-button-renderer style-text"})
+    for tag in divTag:
+        print tag.text
+ #second approch   # for tag in soup.find_all('yt-formatted-string'):
+    #     print tag.text
