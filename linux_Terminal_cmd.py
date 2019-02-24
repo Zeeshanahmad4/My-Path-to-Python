2a7e35c4edb3650df54873c205a5b10825e028f5
Zeeshanahmad4 <ahmadzee26@gmail.com>
2019-02-24 10:27:03 -0500

    Update linux_Terminal_cmd.py

--- linux_Terminal_cmd.py
+++ linux_Terminal_cmd.py
@@ -1,1 +1,33 @@
 
+#knowing current dictonary
+pwd
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Handling AWS with cmd
+
+#connecting with aws
+ssh -i key.pem ubuntu@ec2-18-220-130-227.us-east-2.compute.amazonaws.com
+
+
+#sending files
+scp -i key.pem file_name ubuntu@ec2-18-220-130-227.us-east-2.compute.amazonaws.com:zee
