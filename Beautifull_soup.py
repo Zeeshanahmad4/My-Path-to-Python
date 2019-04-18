e6df167f6cdacb75c31f8acf41a0e1df3df3a6ad
Zeeshanahmad4 <ahmadzee26@gmail.com>
2019-04-18 10:35:39 +0500

    Update Beautifull_soup.py

--- Beautifull_soup.py
+++ Beautifull_soup.py
@@ -1,1 +1,9 @@
 
+#hidden elements that gives null values
+    html = browser.page_source
+    soup = BeautifulSoup(html)
+    divTag = soup.find_all("yt-formatted-string", {"class": "style-scope ytd-toggle-button-renderer style-text"})
+    for tag in divTag:
+        print tag.text
+ #second approch   # for tag in soup.find_all('yt-formatted-string'):
+    #     print tag.text
