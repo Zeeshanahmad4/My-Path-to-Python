f18934af313fb9710e8a3796bd3d815af92c7cbc
Zeeshanahmad4 <ahmadzee26@gmail.com>
2019-06-13 04:52:38 -0400

    Update plotly.py

--- plotly.py
+++ plotly.py
@@ -1,1 +1,24 @@
+import plotly.offline as pyo
+import plotly.graph_objs as go
 
+
+
+#scater graph
+data = [go.Scatter(
+    x = random_x,
+    y = random_y,
+    mode = 'markers',
+)]
+
+
+#desging for title and others
+layout = go.Layout(
+    title = 'Random Data Scatterplot', # Graph title
+    xaxis = dict(title = 'Some random x-values'), # x-axis label
+    yaxis = dict(title = 'Some random y-values'), # y-axis label
+    hovermode ='closest' # handles multiple points landing on the same vertical
+)
+
+#compiling the both layout and data
+fig = go.Figure(data=data, layout=layout)
+pyo.plot(fig, filename='scatter2.html')#ploting
