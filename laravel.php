8849ca2b91c70fbc16c55bfd7768dd66618be5a4
Zeeshanahmad4 <ahmadzee26@gmail.com>
2019-09-15 22:38:41 +0500

    Update laravel.php

--- laravel.php
+++ laravel.php
@@ -15,6 +15,22 @@ composer require laravel/ui
 php artisan ui vue --auth
 
 
+#make middleware
+https://laracasts.com/discuss/channels/general-discussion/create-middleware-to-auth-admin-users?page=0
+
+#Add this to admin 
+use Illuminate\Support\Facades\Auth;
+
+#add this to routing file 
+Route::group(['prefix' => 'backend'], function () {
+    Auth::routes();
+});
+
+Route::group(['prefix' => 'backend', 'middleware' => 'admin'], function () {
+    Route::get('admin-dashboard', 'Backend/AdminController@dashboard');
+});
+
+
 
 php artisan make:controller SiteController  // making controller
 
