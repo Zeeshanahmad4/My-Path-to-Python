c5b62f281d84ea156a6366c7bb42967ebfd81f14
Zeeshanahmad4 <ahmadzee26@gmail.com>
2019-09-15 19:50:09 +0500

    Update laravel.php

--- laravel.php
+++ laravel.php
@@ -1,6 +1,12 @@
+creat Project:
+#putt name of the project in place of blog
+composer create-project --prefer-dist laravel/laravel blog   
+
+
+
+
+
 php artisan make:controller SiteController  // making controller
-
-
 
 
 after git,
