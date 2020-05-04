50423d9061717fc1b8944a03824c8629025a7b54
Zeeshan ahmad <ahmadzee26@gmail.com>
2020-05-04 20:56:38 +0500

    Update and rename multilogin to multilogin.py

---
+++ multilogin.py
@@ -0,0 +1,9 @@
+As I understand you want to disable pop-up blocker?
+Then you can try to add the following line to your app.properties file: chrome.console_arg.disable-popup-blocking=true
+
+You can try to add the following line to your app.properties file:chrome.console_arg.disable-notifications=true
+
+
+
+
+chrome.console_arg.enable-logging=true
