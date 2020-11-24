6c42a1f3aef40ab82cc57974a586bb5dbf8bae0c
Zeeshan ahmad <ahmadzee26@gmail.com>
2020-11-24 16:51:20 +0500

    Update airtable.py

--- airtable.py
+++ airtable.py
@@ -1,1 +1,21 @@
 
+# install it
+pip uninstall airtable
+pip install airtable-python-wrapper
+# import it
+from airtable import Airtable
+
+#intilize
+airtable1 = Airtable(api_key='key7BnLq2O4dACj81',
+                     table_name='leading_ads', base_key='appPzGUWNjmOLCJqG')
+
+#for loop on  list
+for info in airtable1.get_all():
+  print(info['fields'])
+  
+#update record  
+airtable1.update(info['id'], {'Profile_name': 'Ready'})
+
+#replace whole row
+airtable1.replace(record['id'], fields)
+
