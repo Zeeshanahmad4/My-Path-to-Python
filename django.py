722ca6db88d89c845fe6ad944a674fb7ef4ac5f8
Zeeshan Ahmad <ahmadzee26@gmail.com>
2023-08-01 01:59:36 +0500

    Update django.py

--- django.py
+++ django.py
@@ -1,1 +1,8 @@
 
+scp -i /path/to/your/key.pem username@remote:/path/to/remote/file /local/directory
+
+chmod 400 /path/to/your/key.pem
+
+
+createdb -U postgres financebro_local
+pg_restore -d financebro_local -F c -C < /path/to/db_dump.sql
